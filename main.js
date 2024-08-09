993141637632b4d42ca871beb5a519e2c13f7457
Ian Parkinson <h3lloiain@gmail.com>
2024-08-08 23:58:54 -0500

    add header text into array

--- main.js
+++ main.js
@@ -3,5 +3,12 @@ import * as cheerio from 'cheerio';
 const res = await fetch('https://www.freedesktop.org/software/systemd/man/latest/systemd.directives.html')
 const html = await res.text()
 const $ = cheerio.load(html)
+const $h2 = $('h2')
+const headers = [];
 
-console.log($)
+for(let i = 0;i < $h2.length;i++) {
+    if (i<1) continue;
+    headers.push($($h2[i]).text())
+}
+
+console.log(headers)
